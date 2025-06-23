a8a0ded5a71f3a346a1d480164eea913af608248
reiniboy15 <reinhardtj.smit15@gmail.com>
2025-06-23 15:28:16 +0200

    updated performance

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+import constants
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
