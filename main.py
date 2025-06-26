0b704df8a8ac54e74ed5be2cf594cc8039fe14ad
reiniboy15 <reinhardtj.smit15@gmail.com>
2025-06-26 17:04:07 +0200

    Added player

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import pygame
 # and the database_version variable
 # from database.py into the current file
 import constants
+from player import Player
 
 def main():
-    print("Here is change")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -19,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
